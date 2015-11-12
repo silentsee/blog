51039e71f5aef040f2ad7e900ddb88672dbab8d5
silentsee <silientsee@live.com>
2015-11-12 18:29:33 +0800

    添加find_first方法

--- www/transwrap/db.py
+++ www/transwrap/db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import logging, threading
-import functools, time
+import functools, time, uuid
 
 __author__ = 'silentsee'
 
@@ -45,6 +45,18 @@ class Dict(dict):
 
     def __setattr__(self, key, value):
         self[key] = value
+
+
+def next_id(t=None):
+    '''
+    Return next id as 50-char string.
+
+    Args:
+        t: unix timestamp, default to None and using time.time().
+    '''
+    if t is None:
+        t = time.time()
+    return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
 
 
 class DBError(Exception):
