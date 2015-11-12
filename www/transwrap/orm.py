d8bd25f048eec43baa6fec3afebd87a3179b775b
silentsee <silientsee@live.com>
2015-11-12 18:30:01 +0800

    添加find_first方法

--- www/transwrap/orm.py
+++ www/transwrap/orm.py
@@ -5,6 +5,7 @@ import logging
 import db
 import time
 
+
 class Column(object):
     """
     """
@@ -19,12 +20,20 @@ class Column(object):
         self.insertable = kw.get('insertable', True)
         self.updatable = kw.get('updatable', True)
         self._default = kw.get('default', None)
-        self._order = Column._count #用于__sql__()生成语句里，column的顺序固定顺序
+        self._order = Column._count  # 用于__sql__()生成语句里，column的顺序固定顺序
         Column._count += 1
 
     @property
     def default(self):
         return self._default() if callable(self._default) else self._default
+
+    def __str__(self):
+        s = ['< %s: %s %s' % (self.__class__.__name__, self.name, self.default)]
+        self.nullable and s.append('N')
+        self.updatable and s.append('U')
+        self.insertable and s.append('I')
+        s.append('>')
+        return ' '.join(s)
 
 
 class IntegerField(Column):
@@ -58,7 +67,27 @@ class FloatField(Column):
             kw['type'] = 'real'
         super(FloatField, self).__init__(**kw)
 
+
+class BooleanFiled(Column):
+    def __init__(self, **kw):
+        if 'default' not in kw:
+            kw['default'] = False
+        if 'type' not in kw:
+            kw['type'] = 'bool'
+        super(BooleanFiled, self).__init__(**kw)
+
+
+class TextField(Column):
+    def __init__(self,**kw):
+        if 'default' not in kw:
+            kw['default'] = ""
+        if "type" not in kw:
+            kw['type'] = 'text'
+        super(TextField, self).__init__(**kw)
+
+
 _triggers = frozenset(['pre_insert', 'pre_update', 'pre_delete'])
+
 
 class ModelMetaclass(type):
     """
@@ -86,7 +115,7 @@ class ModelMetaclass(type):
             attrs.pop(k)
         attrs['__mappings__'] = mappings  # 在model的类方法里使用
         if not '__table__' in attrs:
-            attrs['__table__'] = name.lower() # 同上
+            attrs['__table__'] = name.lower()  # 同上
         attrs['__primary_key__'] = primary_key
         for t in _triggers:
             if t not in attrs:
@@ -157,7 +186,7 @@ class Model(dict):
         sql.append('-- generating SQL for %s:' % cls.__table__)
         sql.append('create table `%s` (' % cls.__table__)
         for v in sorted(cls.__mappings__.values(), lambda x, y: cmp(x._order, y._order)):
-            sql.append("  `%s` %s %s," %(v.name,v.type, "not null" if  not v.nullable else ""))
+            sql.append("  `%s` %s %s," % (v.name, v.type, "not null" if not v.nullable else ""))
         sql.append('  primary key(`%s`)' % cls.__primary_key__.name)
         sql.append(');')
         return '\n'.join(sql)
@@ -179,7 +208,7 @@ class Model(dict):
         """
         返回cls的对象
         """
-        #return db.select_one('select * from %s where %s =?' % (cls.__table__, cls.__primary_key__.name), primary_key)
+        # return db.select_one('select * from %s where %s =?' % (cls.__table__, cls.__primary_key__.name), primary_key)
         r = db.select_one('select * from %s where %s =?' % (cls.__table__, cls.__primary_key__.name), primary_key)
         return cls(**r) if r else None
 
@@ -198,20 +227,28 @@ class Model(dict):
 
     @classmethod
     def find_all(cls):
-        #return db.select('select * from `%s`' % cls.__table__) #这样写返回的不是User或者具体的表对象,而是由dict组成的list
+        # return db.select('select * from `%s`' % cls.__table__) #这样写返回的不是User或者具体的表对象,而是由dict组成的list
         l = db.select('select * from `%s`' % cls.__table__)
-        return [ cls(**d) for d in l ] #这样写，返回的就是由对象组成的list
+        return [cls(**d) for d in l]  # 这样写，返回的就是由对象组成的list
+
+    @classmethod
+    def find_first(cls, where, *args):
+        """
+        User.find_first('where id=?', 9999)
+        :return cls() or None
+        """
+        r = db.select_one('select * from %s %s' % (cls.__table__, where), *args)
+        return cls(**r) if r else None
 
     def delete(self):
-        self.pre_delete and self.pre_delete() #这里用一种逻辑短路的写法，若pre_delete为空，则跳过
+        self.pre_delete and self.pre_delete()  # 这里用一种逻辑短路的写法，若pre_delete为空，则跳过
         pk = self.__primary_key__.name
-        db.delete('delete from `%s` where %s=?' % (self.__table__, self.__primary_key__.name), getattr(self,pk))
+        db.delete('delete from `%s` where %s=?' % (self.__table__, self.__primary_key__.name), getattr(self, pk))
         return self
 
-
-
+"""
 class User(Model):
-    """
+
     如果我在User里写了insert方法，get方法，delete方法等
     那么如果有一个的表，比如Blog表，那么我仍然要写一个同样的方法，这些方法的参数都是一样的，都是列名值对来表示的记录。
     此时有以下选择：
@@ -231,19 +268,20 @@ class User(Model):
     要这样写则该对象负责实现insert等方法，而方法调用在user等类上，该类为user，blog的父类
     那么，父类如何获取子类的字段呢？
     使用元类
-    """
+
     __table__ = 'user'
     id = IntegerField(primary_key=True)  # uuid形式
     name = StringField()  # 存储char
     email = StringField()  # 存储char
     passwd = StringField()  # 存储char TODO：默认必须非明文存储
     last_modified = FloatField()
-
+"""
 
 if __name__ == '__main__':  # 测试时运行
-    logging.basicConfig(level=logging.DEBUG)  # 将日志级别设为debug
+    logging.basicConfig(level=logging.CRITICAL)  # 将日志级别设为debug
     db.create_engine('root', 'root', 'test')  # 创建数据库引擎
     db.update('drop table if exists user')  # 创建user表用来测试
     db.update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
     import doctest
+
     doctest.testmod()
