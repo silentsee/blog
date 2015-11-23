32605a678ac2f1e91ed9a68572197483c0787e0d
silentsee <silientsee@live.com>
2015-11-23 22:16:24 +0800

    增加前端页面

--- www/urls.py
+++ www/urls.py
@@ -5,8 +5,19 @@ from models import Blog, User, Comment
 from transwrap.web import  view, get
 
 
-@view('test_users.html')
-@get('/')
-def test_users():
+
+@view("test_users.html")
+@get('/test/:id')
+def getbyid(id):
+    user = User.get(primary_key=id)
     users = User.find_all()
-    return dict(users=users)
+    return dict(users=users, user=user)
+
+
+@view('blogs.html')
+@get('/')
+def index():
+    blogs = Blog.find_all()
+    # 查找登陆用户:
+    user = User.find_first('where email=?', 'admin@example.com')
+    return dict(blogs=blogs, user=user)
